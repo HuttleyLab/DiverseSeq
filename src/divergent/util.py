c81d20712879138e34a024ee9554a72e7c44158b
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-09-27 15:02:47 +1000

    ENH: new seq loader mechyanism
    [NEW] util.filename_seqname class stores names of files
        and sequence labels.
    
    [NEW] util.faster_load_fasta() app uses above. It also checks
        for duplicate sequence labels within files, warning about them
        when the sequence is different.

--- src/divergent/util.py
+++ src/divergent/util.py
@@ -1,11 +1,13 @@
 import pickle
+import warnings
 
 from pathlib import Path
 from typing import Any, Union
 
 import blosc2
 
-from cogent3 import make_seq, open_
+from attrs import define
+from cogent3 import make_seq, make_table, open_
 from cogent3.app import composable
 from cogent3.app import typing as c3_types
 from cogent3.parse.fasta import MinimalFastaParser
@@ -13,6 +15,21 @@ from cogent3.parse.fasta import MinimalFastaParser
 
 def _label_func(label):
     return label.split()[0]
+
+
+@composable.define_app
+def faster_load_fasta(path: c3_types.IdentifierType, label_func=_label_func) -> dict:
+    with open_(path) as infile:
+        result = {}
+        for n, s in MinimalFastaParser(infile.read().splitlines()):
+            n = label_func(n)
+            if n in result and result[n] != s:
+                warnings.warn(
+                    f"duplicated seq label {n!r} in {path}, but different seqs",
+                    UserWarning,
+                )
+            result[n] = s
+        return result
 
 
 @composable.define_app(app_type=composable.LOADER)
@@ -56,22 +73,37 @@ def unpickle_data(data: bytes) -> Any:
     return pickle.loads(data)
 
 
+@define
+class filename_seqname:
+    source: str
+    name: str
+
+
 @composable.define_app(app_type="loader")
 class seq_from_fasta:
     def __init__(self, label_func=_label_func, max_length=None) -> None:
-        self.label_func = label_func
         self.max_length = max_length
+        self.loader = faster_load_fasta(label_func=label_func)
 
-    def main(self, path: c3_types.IdentifierType) -> c3_types.SeqType:
-        with open_(path) as infile:
-            data = list(iter(MinimalFastaParser(infile.read().splitlines())))
-            assert len(data) == 1
-            name, seq = data[0]
-            name = self.label_func(name)
+    def main(self, identifier: filename_seqname) -> c3_types.SeqType:
+        name = identifier.name
+        data = self.loader(identifier.source)
+        seq = data[name]
 
         if self.max_length:
             seq = seq[: self.max_length]
 
         seq = make_seq(seq, name=name, moltype="dna")
-        seq.source = str(path)
+        seq.source = identifier.source
         return seq
+
+
+def get_seq_identifiers(paths, label_func=_label_func) -> list[filename_seqname]:
+    loader = faster_load_fasta(label_func=label_func)
+    records = []
+    for path in paths:
+        seq_names = list(loader(path))
+        records.extend(filename_seqname(str(path), n) for n in seq_names)
+    return records
+
+
