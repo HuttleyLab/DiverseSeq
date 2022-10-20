5b55324001845ada4b9639aaee915e1caa047720
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2022-10-20 14:01:57 +1100

    ENH: added module and tests for k-mer distance calculations

---
+++ src/divergent/distance.py
@@ -0,0 +1,35 @@
+from functools import singledispatch
+
+from numpy import intersect1d, ndarray, union1d
+
+from divergent.record import unique_kmers
+
+
+@singledispatch
+def _intersect_union(rec1, rec2) -> tuple[int, int]:
+    raise NotImplementedError
+
+
+@_intersect_union.register(unique_kmers)
+def _(rec1: unique_kmers, rec2: unique_kmers) -> tuple[int, int]:
+    return _intersect_union(rec1.data, rec2.data)
+
+
+@_intersect_union.register(set)
+def _(rec1: set, rec2: set) -> tuple[int, int]:
+    intersect = rec1 & rec2
+    union = rec1 | rec2
+    return len(intersect), len(union)
+
+
+@_intersect_union.register(ndarray)
+def _(rec1: ndarray, rec2: ndarray) -> tuple[int, int]:
+    intersect = intersect1d(rec1, rec2, assume_unique=True)
+    union = union1d(rec1.data, rec2.data)
+    return len(intersect), len(union)
+
+
+def jaccard(rec1, rec2) -> float:
+    """returns the Jaccard distance between rec1 and rec2"""
+    i, u = _intersect_union(rec1, rec2)
+    return 1 - (i / u)
---
+++ tests/test_distance.py
@@ -0,0 +1,37 @@
+import pytest
+
+from numpy import array
+from numpy.testing import assert_allclose
+
+from divergent.distance import _intersect_union, jaccard
+from divergent.record import unique_kmers
+
+
+def _make_sample(type_):
+    if type_ != unique_kmers:
+        return type_([0, 1, 3]), type_([1, 4, 5])
+
+    i, u = _make_sample(array)
+    kwargs = dict(num_states=4, k=1)
+    return unique_kmers(data=i, **kwargs), unique_kmers(data=u, **kwargs)
+
+
+@pytest.mark.parametrize(
+    "rec1,rec2", (_make_sample(set), _make_sample(array), _make_sample(unique_kmers))
+)
+def test_intersect_union(rec1, rec2):
+    i, u = _intersect_union(rec1, rec2)
+    assert i == 1 and u == 5
+
+
+@pytest.mark.parametrize(
+    "rec1,rec2,exp",
+    (
+        [{1, 2}, {3, 4}, 1],
+        [set(), {1}, 1],
+        [{1, 2}, {1, 2}, 0],
+        [{1, 2}, {1, 3}, 2 / 3],
+    ),
+)
+def test_jaccard(rec1, rec2, exp):
+    assert_allclose(jaccard(rec1, rec2), exp)
