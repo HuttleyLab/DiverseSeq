3c8053510f525d1ceb5e199dea825f377cc5e657
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2023-01-20 09:58:42 +1100

    DEV: updated the noxfile

--- noxfile.py
+++ noxfile.py
@@ -8,25 +8,17 @@ _py_versions = range(8, 11)
 
 @nox.session(python=[f"3.{v}" for v in _py_versions])
 def test(session):
-    py_version = session.python.replace(".", "")
     session.install(".[test]")
     session.chdir("tests")
     session.run(
         "pytest",
         "-x",
-        "--junitxml",
-        f"junit-{py_version}.xml",
-        "--cov-report",
-        "xml",
-        "--cov",
-        "divergent",
     )
 
 
 @nox.session(python=["3.10"])
 def testcov(session):
-    session.install(*dependencies)
-    session.install(".")
+    session.install(".[test]")
     session.chdir("tests")
     session.run(
         "pytest",
