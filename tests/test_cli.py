149c34d2de001ede1e4641684c2f893b73db76e9
KatherineCaley <u6096186@anu.edu.au>
2024-03-13 10:20:54 +1100

    TST: updated tests to use processed data

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -26,8 +26,12 @@ def runner():
 
 
 @pytest.fixture(scope="session")
-def seq_path():
+def fasta_seq_path():
     return DATADIR / "brca1.fasta"
+
+@pytest.fixture(scope="session")
+def h5_seq_path():
+    return DATADIR / "brca1.h5"
 
 
 def _checked_output(path, eval_rows=None, eval_header=None):
@@ -40,52 +44,52 @@ def _checked_output(path, eval_rows=None, eval_header=None):
         assert eval_header(table.header)
 
 
-def test_defaults(runner, tmp_dir, seq_path):
+def test_defaults(runner, tmp_dir, h5_seq_path):
     outpath = tmp_dir / "test_defaults.tsv"
-    args = f"-s {seq_path} -o {outpath}".split()
+    args = f"-s {h5_seq_path} -o {outpath}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
     _checked_output(str(outpath))
 
 
 @pytest.mark.parametrize("min_size", (2, 5))
-def test_min_size(runner, tmp_dir, seq_path, min_size):
+def test_min_size(runner, tmp_dir, h5_seq_path, min_size):
     outpath = tmp_dir / "test_min_size.tsv"
-    args = f"-s {seq_path} -o {outpath} -z {min_size}".split()
+    args = f"-s {h5_seq_path} -o {outpath} -z {min_size}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
     _checked_output(outpath, eval_rows=lambda x: x >= min_size)
 
 
 @pytest.mark.parametrize("max_size", (5, 7))
-def test_max_size(runner, tmp_dir, seq_path, max_size):
+def test_max_size(runner, tmp_dir, h5_seq_path, max_size):
     outpath = tmp_dir / "test_max_size.tsv"
-    args = f"-s {seq_path} -o {outpath} -z 3 -zp {max_size}".split()
+    args = f"-s {h5_seq_path} -o {outpath} -z 3 -zp {max_size}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
     _checked_output(outpath, eval_rows=lambda x: x <= max_size)
 
 
 @pytest.mark.parametrize("size", (5, 7))
-def test_min_eq_max(runner, tmp_dir, seq_path, size):
+def test_min_eq_max(runner, tmp_dir, h5_seq_path, size):
     outpath = tmp_dir / "test_min_eq_max.tsv"
-    args = f"-s {seq_path} -o {outpath} -z {size} -zp {size}".split()
+    args = f"-s {h5_seq_path} -o {outpath} -z {size} -zp {size}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
     _checked_output(outpath, eval_rows=lambda x: x == size)
 
 
-def test_min_gt_max_fail(runner, tmp_dir, seq_path):
+def test_min_gt_max_fail(runner, tmp_dir, h5_seq_path):
     outpath = tmp_dir / "test_min_gt_max_fail.tsv"
-    args = f"-s {seq_path} -o {outpath} -zp 2".split()
+    args = f"-s {h5_seq_path} -o {outpath} -zp 2".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code != 0, r.output
 
 
 @pytest.mark.parametrize("stat", ("total_jsd", "mean_jsd", "mean_delta_jsd"))
-def test_stat(runner, tmp_dir, seq_path, stat):
+def test_stat(runner, tmp_dir, h5_seq_path, stat):
     outpath = tmp_dir / "test_defaults.tsv"
-    args = f"-s {seq_path} -o {outpath} -st {stat}".split()
+    args = f"-s {h5_seq_path} -o {outpath} -st {stat}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
 
@@ -93,9 +97,9 @@ def test_stat(runner, tmp_dir, seq_path, stat):
 
 
 @pytest.mark.parametrize("k", (1, 3, 7))
-def test_k(runner, tmp_dir, seq_path, k):
+def test_k(runner, tmp_dir, h5_seq_path, k):
     outpath = tmp_dir / "test_defaults.tsv"
-    args = f"-s {seq_path} -o {outpath} -k {k}".split()
+    args = f"-s {h5_seq_path} -o {outpath} -k {k}".split()
     r = runner.invoke(dvgt_max, args, catch_exceptions=False)
     assert r.exit_code == 0, r.output
     _checked_output(outpath)
