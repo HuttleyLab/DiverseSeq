255c54d5652c3d502067ce6eba6d9effd6d756d9
KatherineCaley <u6096186@anu.edu.au>
2024-04-12 15:23:20 +1000

    DEV: gut max command and port to apps

--- src/divergent/loader.py
+++ src/divergent/loader.py
@@ -1,16 +1,27 @@
 import warnings
 
 from pathlib import Path
+from typing import Optional, Union
+
+import h5py
 
 from attrs import define
 from cogent3 import make_seq, make_table, open_
 from cogent3.app import typing as c3_types
 from cogent3.app.composable import LOADER, define_app
-from cogent3.app.data_store import DataMember
+from cogent3.app.data_store import (
+    OVERWRITE,
+    DataMember,
+    DataStoreABC,
+    DataStoreDirectory,
+    Mode,
+)
+from cogent3.format.fasta import alignment_to_fasta
 from cogent3.parse.fasta import MinimalFastaParser
 
 from divergent import util as dv_utils
-from divergent.record import SeqArray
+from divergent.data_store import HDF5DataStore
+from divergent.record import SeqArray, SeqRecord
 
 
 def _label_func(label):
@@ -49,43 +60,38 @@ def faster_load_fasta(path: c3_types.IdentifierType, label_func=_label_func) -> 
             result[n] = s.replace("-", "")
         return result
 
-
-@define_app(app_type=LOADER)
-class dvgt_load_seqs:
-    def __init__(self, moltype: str = "dna"):
-        """load fasta sequences from a data store
-
-        Parameters
-        ----------
-        moltype
-            molecular type
-
-        Notes
-        -----
-        Assumes each .fa file contains a single sequence so only takes the first
-        """
-        self.moltype = moltype
-        self.str2arr = dv_utils.str2arr(moltype=self.moltype)
-
-    def main(self, data: DataMember) -> SeqArray:
-        path_to_seq = Path(data.data_store.source) / data.unique_id
-
-        with open_(path_to_seq) as infile:
-            for _, seq in MinimalFastaParser(infile.read().splitlines()):
-                seq.replace("-", "")
-
-        return SeqArray(
-            seqid=data.unique_id,
-            data=self.str2arr(seq),
-            moltype=self.moltype,
-            source=data.data_store.source,
-        )
-
-
 @define
 class filename_seqname:
     source: str
     name: str
+
+
+def get_seq_identifiers(paths, label_func=_label_func) -> list[filename_seqname]:
+    loader = faster_load_fasta(label_func=label_func)
+    records = []
+    for path in paths:
+        seq_names = list(loader(path))
+        records.extend(filename_seqname(str(path), n) for n in seq_names)
+    return records
+
+
+@define_app(app_type=LOADER)
+class concat_seqs:
+    def __init__(
+        self, label_func: callable = _label_from_filename, max_length: int | None = None
+    ) -> None:
+        self.label_func = label_func
+        self.max_length = max_length
+        self.loader = faster_load_fasta(label_func=label_func)
+
+    def main(self, path: c3_types.IdentifierType) -> c3_types.SeqType:
+        data = self.loader(path)
+        seq = "-".join(data.values())
+
+        if self.max_length:
+            seq = seq[: self.max_length]
+
+        return make_seq(seq, name=self.label_func(path), moltype="dna")
 
 
 @define_app(app_type=LOADER)
@@ -117,29 +123,91 @@ class seqarray_from_fasta:
         )
 
 
-def get_seq_identifiers(paths, label_func=_label_func) -> list[filename_seqname]:
-    loader = faster_load_fasta(label_func=label_func)
-    records = []
-    for path in paths:
-        seq_names = list(loader(path))
-        records.extend(filename_seqname(str(path), n) for n in seq_names)
-    return records
+@define_app
+class dvgt_seq_file_to_data_store:
+    def __init__(
+        self,
+        dest: Optional[c3_types.IdentifierType] = None,
+        limit: Optional[int] = None,
+        mode: Union[str, Mode] = OVERWRITE,
+    ):
+        self.dest = dest
+        self.limit = limit
+        self.mode = mode
+        self.loader = faster_load_fasta()
+
+    def main(self, fasta_path: c3_types.IdentifierType) -> DataStoreABC:
+        outpath = Path(self.dest) if self.dest else Path(fasta_path).with_suffix("")
+        outpath.mkdir(parents=True, exist_ok=True)
+        out_dstore = DataStoreDirectory(
+            source=outpath, mode=self.mode, suffix=".fa", limit=self.limit
+        )
+
+        seqs = self.loader(fasta_path)
+
+        for seq_id, seq_data in seqs.items():
+            fasta_seq_data = alignment_to_fasta({seq_id: seq_data}, block_size=80)
+            out_dstore.write(unique_id=seq_id, data=fasta_seq_data)
+
+        return out_dstore
 
 
 @define_app(app_type=LOADER)
-class concat_seqs:
-    def __init__(
-        self, label_func: callable = _label_from_filename, max_length: int | None = None
-    ) -> None:
-        self.label_func = label_func
-        self.max_length = max_length
-        self.loader = faster_load_fasta(label_func=label_func)
+class dvgt_load_seqs:
+    def __init__(self, moltype: str = "dna"):
+        """load fasta sequences from a data store
 
-    def main(self, path: c3_types.IdentifierType) -> c3_types.SeqType:
-        data = self.loader(path)
-        seq = "-".join(data.values())
+        Parameters
+        ----------
+        moltype
+            molecular type
 
-        if self.max_length:
-            seq = seq[: self.max_length]
+        Notes
+        -----
+        Assumes each fasta file contains a single sequence so only takes the first
+        """
+        self.moltype = moltype
+        self.str2arr = dv_utils.str2arr(moltype=self.moltype)
 
-        return make_seq(seq, name=self.label_func(path), moltype="dna")
+    def main(self, data_member: DataMember) -> SeqArray:
+        seq_path = Path(data_member.data_store.source) / data_member.unique_id
+
+        with open_(seq_path) as infile:
+            for _, seq in MinimalFastaParser(infile.read().splitlines()):
+                seq.replace("-", "")
+
+        return SeqArray(
+            seqid=data_member.unique_id,
+            data=self.str2arr(seq),
+            moltype=self.moltype,
+            source=data_member.data_store.source,
+        )
+
+
+@define_app(app_type=LOADER)
+def dvgt_load_prepped_seqs(data_member: DataMember) -> SeqArray:
+    """loads prepped sequences from a dvgtseqs data store"""
+    seq = data_member.data_store.read(unique_id=data_member.unique_id)
+    attrs = data_member.data_store.get_attrs(unique_id=data_member.unique_id)
+    moltype = attrs.get("moltype", None)
+    source = attrs.get("source", None)
+
+    return SeqArray(
+        seqid=data_member.unique_id, data=seq, moltype=moltype, source=source
+    )
+
+
+@define_app(app_type=LOADER)
+def dvgt_load_records(dstore: HDF5DataStore) -> SeqArray:
+    """loads prepped sequences from a dvgtseqs data store"""
+    records = []
+    for data_member in dstore.members:
+        name = data_member.unique_id
+        data = data_member.data_store.read(data_member.unique_id)
+        attrs = data_member.data_store.get_attrs(unique_id=data_member.unique_id)
+        length = int(attrs.get("length", None))
+
+        records.append(
+            SeqRecord(kcounts=data, name=name, length=length)
+        )
+    return records
--- src/divergent/writer.py
+++ src/divergent/writer.py
@@ -5,7 +5,7 @@ from cogent3.app.composable import WRITER, define_app
 from cogent3.app.data_store import get_unique_id
 
 from divergent.data_store import HDF5DataStore
-from divergent.record import SeqArray
+from divergent.record import SeqArray, SeqRecord
 
 
 @define_app(app_type=WRITER)
@@ -14,18 +14,36 @@ class dvgt_write_prepped_seqs:
         self,
         data_store: HDF5DataStore,
         id_from_source: callable = get_unique_id,
-        moltype: str = None,
     ):
         self.data_store = data_store
         self.id_from_source = id_from_source
-        self.moltype = moltype
 
     def main(
         self, data: SeqArray, identifier: Optional[str] = None
     ) -> c3_types.IdentifierType:
+        unique_id = identifier or self.id_from_source(data.unique_id)
         return self.data_store.write(
-            unique_id=identifier,
+            unique_id=unique_id,
             data=data.data,
             moltype=data.moltype,
             source=str(data.source),
+        )
+
+
+@define_app(app_type=WRITER)
+class dvgt_write_record:
+    def __init__(
+        self,
+        data_store: HDF5DataStore,
+    ):
+        self.data_store = data_store
+
+    def main(
+        self, data: SeqRecord, identifier: Optional[str] = None
+    ) -> c3_types.IdentifierType:
+        unique_id = identifier or data.name
+        return self.data_store.write(
+            unique_id=unique_id,
+            data=data.kcounts.data,
+            length=data.length,
         )
