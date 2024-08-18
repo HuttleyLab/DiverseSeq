a8e7f89b22f9a0ae9b81db2a33b5c5042b940550
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-08-18 10:24:23 +1000

    ENH: Hdf5 data_store supports serialisation
    [NEW] becomes read only if pickled
    
    [CHANGED] the file is now always open, until it's explicitly closed.
        This is a precursor to supporting in in-memory data store.

--- src/divergent/data_store.py
+++ src/divergent/data_store.py
@@ -1,3 +1,6 @@
+import contextlib
+import inspect
+import os
 import pathlib
 
 import h5py
@@ -24,6 +27,16 @@ class HDF5DataStore(DataStoreABC):
     stored as attributed on data sets.
     """
 
+    def __new__(cls, *args, **kwargs):
+        obj = object.__new__(cls)
+        init_sig = inspect.signature(cls.__init__)
+        bargs = init_sig.bind_partial(cls, *args, **kwargs)
+        bargs.apply_defaults()
+        init_vals = bargs.arguments
+        init_vals.pop("self", None)
+        obj._init_vals = init_vals
+        return obj
+
     def __init__(
         self,
         source: str | pathlib.Path,
@@ -33,6 +46,23 @@ class HDF5DataStore(DataStoreABC):
         self._source = pathlib.Path(source)
         self._mode = Mode(mode)
         self._limit = limit
+        self._file = h5py.File(source, mode=self.mode.name, **self._h5_kwargs)
+        self._is_open = True
+        self._completed = []
+
+    def __getstate__(self):
+        init_vals = self._init_vals.copy()
+        init_vals["mode"] = "r"
+        return init_vals
+
+    def __setstate__(self, state):
+        obj = self.__class__(**state)
+        self.__dict__.update(obj.__dict__)
+        # because we have a __del__ method, and self attributes point to
+        # attributes on obj, we need to modify obj state so that garbage
+        # collection does not screw up self
+        obj._is_open = False
+        obj._file = None
 
     @property
     def source(self) -> pathlib.Path:
@@ -50,19 +80,15 @@ class HDF5DataStore(DataStoreABC):
 
     def read(self, unique_id: str) -> numpy.ndarray:
         """reads and return array data corresponding to identifier"""
-        with h5py.File(self._source, mode="r") as f:
-            # TODO: this will fail if the unique_id is not in the file
-            data = f[unique_id]
-            out = numpy.empty(len(data), dtype=numpy.uint8)
-            data.read_direct(out)
+        data = self._file[unique_id]
+        out = numpy.empty(len(data), dtype=numpy.uint8)
+        data.read_direct(out)
         return out
 
     def get_attrs(self, unique_id: str) -> dict:
         """return all data set attributes connected to an identifier"""
-        with h5py.File(self._source, mode="r") as f:
-            data = f[unique_id]
-            attrs = dict(data.attrs.items())
-        return attrs
+        data = self._file[unique_id]
+        return dict(data.attrs.items())
 
     def _write(
         self,
@@ -72,27 +98,28 @@ class HDF5DataStore(DataStoreABC):
         data: numpy.ndarray,
         **kwargs,
     ) -> DataMember:
-        with h5py.File(self._source, mode="a") as f:
-            path = f"{subdir}/{unique_id}" if subdir else unique_id
-            dset = f.create_dataset(path, data=data, dtype="u1", **_HDF5_BLOSC2_KWARGS)
+        f = self._file
+        path = f"{subdir}/{unique_id}" if subdir else unique_id
+        dset = f.create_dataset(path, data=data, dtype="u1", **_HDF5_BLOSC2_KWARGS)
 
-            if subdir == _LOG_TABLE:
-                return None
+        if subdir == _LOG_TABLE:
+            return None
 
-            if subdir == _NOT_COMPLETED_TABLE:
-                member = DataMember(
-                    data_store=self,
-                    unique_id=pathlib.Path(_NOT_COMPLETED_TABLE) / unique_id,
-                )
+        if subdir == _NOT_COMPLETED_TABLE:
+            member = DataMember(
+                data_store=self,
+                unique_id=pathlib.Path(_NOT_COMPLETED_TABLE) / unique_id,
+            )
 
-            elif not subdir:
-                member = DataMember(data_store=self, unique_id=unique_id)
-                for key, value in kwargs.items():
-                    dset.attrs[key] = value
+        elif not subdir:
+            member = DataMember(data_store=self, unique_id=unique_id)
+            for key, value in kwargs.items():
+                dset.attrs[key] = value
 
-            md5 = get_text_hexdigest(data.tobytes())
-            md5_dtype = h5py.string_dtype()
-            f.create_dataset(f"{_MD5_TABLE}/{unique_id}", data=md5, dtype=md5_dtype)
+        md5 = get_text_hexdigest(data.tobytes())
+        md5_dtype = h5py.string_dtype()
+        f.create_dataset(f"{_MD5_TABLE}/{unique_id}", data=md5, dtype=md5_dtype)
+        f.flush()
         return member
 
     def write(self, *, unique_id: str, data: numpy.ndarray, **kwargs) -> DataMember:
@@ -106,8 +133,9 @@ class HDF5DataStore(DataStoreABC):
         data
             The data to be stored in the record.
         kwargs
-            Any additional keyword arguments will be stored as attributes on the dataset.
-            Each key-value pair in kwargs corresponds to an attribute name and its value.
+            Any additional keyword arguments will be stored as attributes on the
+            dataset. Each key-value pair in kwargs corresponds to an attribute
+            name and its value.
 
         Returns
         -------
@@ -131,24 +159,21 @@ class HDF5DataStore(DataStoreABC):
     def drop_not_completed(self, *, unique_id: str | None = None) -> None: ...
 
     def md5(self, unique_id: str) -> str | None:
-        with h5py.File(self._source, mode="a") as f:
-            if f"md5/{unique_id}" in f:
-                dset = f[f"md5/{unique_id}"]
-                md5 = dset[()].decode("utf-8")
-            else:
-                md5 = None
-        return md5
+        f = self._file
+        if f"md5/{unique_id}" in f:
+            dset = f[f"md5/{unique_id}"]
+            return dset[()].decode("utf-8")
+        else:
+            return None
 
     @property
     def completed(self) -> list[DataMember]:
         if not self._completed:
-            self._completed = []
-            with h5py.File(self._source, mode="a") as f:
-                self._completed.extend(
-                    DataMember(data_store=self, unique_id=name)
-                    for name, item in f.items()
-                    if isinstance(item, h5py.Dataset)
-                )
+            self._completed = [
+                DataMember(data_store=self, unique_id=name)
+                for name in self._file.keys()
+                if name not in (_LOG_TABLE, _NOT_COMPLETED_TABLE, _MD5_TABLE)
+            ]
         return self._completed
 
     @property
@@ -158,6 +183,31 @@ class HDF5DataStore(DataStoreABC):
     @property
     def not_completed(self) -> list[DataMember]:
         return []
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        """closes the hdf5 file"""
+        # hdf5 dumps content to stdout if resource already closed, so
+        # we trap that here, and capture expected exceptions raised in the
+        # process
+        try:
+            open
+        except NameError:
+            # builtin open() already garbage collected, so nothing to do
+            return
+        with open(os.devnull, "w") as devnull:
+            with (
+                contextlib.redirect_stderr(devnull),
+                contextlib.redirect_stdout(devnull),
+            ):
+                with contextlib.suppress(ValueError, AttributeError):
+                    if self._is_open:
+                        self._file.flush()
+
+                with contextlib.suppress(AttributeError):
+                    self._file.close()
 
 
 def get_seqids_from_store(
