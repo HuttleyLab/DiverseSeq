a636cb1c3d24045658b77e845d0faaa21552a20d
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-08-19 13:34:27 +1000

    DEV: put the hdf5plugin import in __init__
    [FIXED] ability of h5py to find plugin directory requires this
        module be imported, easiest to do this centrally

--- src/divergent/__init__.py
+++ src/divergent/__init__.py
@@ -1,3 +1,7 @@
 """divergent: a tool for sampling biological sequences"""
 
+# need to import hdf5plugin here to make sure the plugin path can be
+# found by h5py
+import hdf5plugin  # noqa
+
 __version__ = "2024.8"
