ccac81e92a688d5c3b4cbdadb672bbe79c5667fc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-08-22 17:15:21 +1000

    ENHL simplfy the benchmark script

--- paper/nbks/benchmark.py
+++ paper/nbks/benchmark.py
@@ -1,63 +1,43 @@
+import os
+import shutil
+import tempfile
 import time
-from itertools import product
 from pathlib import Path
 
 import click
-from cogent3 import make_seq, make_table
-from cogent3.app.composable import define_app
-from cogent3.util import parallel as PAR
-from numpy.random import shuffle
-from rich.progress import track
+from click.testing import CliRunner
+from cogent3 import make_table
+from rich import progress as rich_progress
 
+from divergent import cli as dvgt_cli
 from divergent import util as dvgt_util
-from divergent.record import seq_to_record
-from divergent.records import max_divergent
+
+RUNNER = CliRunner()
 
 
-def _load_all_seqs(seqdir, limit):
-    fasta_paths = list(seqdir.glob("*.fa*"))
-    if limit:
-        fasta_paths = fasta_paths[:limit]
-    loader = faster_load_fasta()
-    seqs = []
-    for r in track(
-        PAR.as_completed(loader, fasta_paths, max_workers=6),
-        total=len(fasta_paths),
-        description="Load seqs",
-    ):
-        name = list(r)[0]
-        seqs.append(make_seq(r[name], name=name))
-    return seqs
+class TempWorkingDir:
+    def __enter__(self):
+        self.original_directory = os.getcwd()
+        self.temp_directory = tempfile.mkdtemp()
+        os.chdir(self.temp_directory)
+        return Path(self.temp_directory)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        os.chdir(self.original_directory)
+        shutil.rmtree(self.temp_directory)
 
 
-def time_seq2rec(seqs, k):
-    # convert to records
-    start = time.time()
-    s2r = seq_to_record(k=k, moltype="dna")
-    records = [s2r(s) for s in seqs]
-    end = time.time()
-    return end - start, records
+class TimeIt:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
 
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.end_time = time.time()
+        self.elapsed_time = self.end_time - self.start_time
 
-def time_max_divergent(records):
-    # convert to records
-    start = time.time()
-    result = max_divergent(records, min_size=7, max_size=12, stat="mean_jsd")
-    end = time.time()
-    return end - start
-
-
-@define_app
-class timed_run:
-    def __init__(self, seqs):
-        self.seqs = seqs
-
-    def main(self, settings: tuple[int]) -> tuple:
-        k, num_seqs, rep = settings
-        shuffle(self.seqs)
-        s2r_time, records = time_seq2rec(self.seqs[:num_seqs], k=k)
-        md_time = time_max_divergent(records)
-        return (k, num_seqs, rep, s2r_time, md_time)
+    def get_elapsed_time(self):
+        return self.elapsed_time
 
 
 _click_command_opts = dict(
@@ -66,45 +46,75 @@ _click_command_opts = dict(
 )
 
 
-@click.group(**_click_command_opts)
-def main():
-    """benchmarking exercises"""
+def run_prep(temp_dir, seqdir, dvgt_file, suffix, num_seqs):
+    # run the prep command
+    args = f"-s {seqdir} -o {dvgt_file} -sf {suffix} -L {num_seqs} -np 1".split()
+    with TimeIt() as timer:
+        r = RUNNER.invoke(dvgt_cli.prep, args, catch_exceptions=False)
+        assert r.exit_code == 0, r.output
+    return timer.get_elapsed_time()
 
 
-@main.command(**_click_command_opts)
+def run_max(seqfile, outpath, k):
+    args = f"-s {seqfile} -o {outpath} --min_size 5 --max_size 10 -k {k} -np 1".split()
+    with TimeIt() as timer:
+        r = RUNNER.invoke(dvgt_cli.max, args, catch_exceptions=False)
+        assert r.exit_code == 0, r.output
+    return timer.get_elapsed_time()
+
+
+@click.command(**_click_command_opts)
 @click.argument("seqdir", type=Path)
 @click.argument("outpath", type=Path)
-@click.option("-L", "--limit", type=int)
-@click.option("-P", "--parallel", is_flag=True)
-def run(seqdir, outpath, limit, parallel):
+@click.option("-s", "--suffix", type=str, default="gb")
+def run(seqdir, suffix, outpath):
+    seqdir = seqdir.absolute()
+    kmer_sizes = list(range(2, 9))
+    num_seqs = list(range(50, 201, 50))
+    results = []
     with dvgt_util.keep_running():
-        settings = list(product(range(3, 8, 2), range(50, 201, 50), range(5)))
-        if limit:
-            settings = settings[:5]
-        seqs = _load_all_seqs(seqdir, limit)
-        app = timed_run(seqs)
-        series = (
-            PAR.as_completed(app, settings, max_workers=6)
-            if parallel
-            else map(app, settings)
-        )
-        rows = list(track(series, total=len(settings)))
-        table = make_table(
-            header=["k", "num_seqs", "rep", "time(s2r)", "time(maxd)"],
-            data=rows,
-        )
-        table.write(outpath)
+        with rich_progress.Progress(
+            rich_progress.TextColumn("[progress.description]{task.description}"),
+            rich_progress.BarColumn(),
+            rich_progress.TaskProgressColumn(),
+            rich_progress.TimeRemainingColumn(),
+            rich_progress.TimeElapsedColumn(),
+        ) as progress:
+            repeats = progress.add_task("Doing reps", total=1)
+            for _ in range(3):
+                seqnum = progress.add_task("Doing num seqs", total=len(num_seqs))
+                for num in num_seqs:
+                    with TempWorkingDir() as temp_dir:
+                        dvgt_file = temp_dir / f"dvgt_L{num}.dvgtseqs"
+                        elapsed_time = run_prep(
+                            temp_dir,
+                            seqdir,
+                            dvgt_file,
+                            suffix,
+                            num,
+                        )
+                        results.append(("prep", num, None, elapsed_time))
 
+                        kmers = progress.add_task(
+                            "Doing kmers",
+                            total=len(kmer_sizes),
+                            transient=True,
+                        )
+                        for k in kmer_sizes:
+                            kout = temp_dir / f"selected-{k}.tsv"
+                            elapsed_time = run_max(dvgt_file, kout, k)
+                            results.append(("max", num, k, elapsed_time))
+                            progress.update(kmers, advance=1, refresh=True)
+                        progress.remove_task(kmers)
 
-@main.command(**_click_command_opts)
-@click.argument("seqdir", type=Path)
-def profile(seqdir):
-    settings = 7, 10, 1
-    seqs = _load_all_seqs(seqdir, 100)
-    app = timed_run(seqs)
-    result = app(settings)
-    print(result)
+                        progress.update(seqnum, advance=1, refresh=True)
+                progress.remove_task(seqnum)
+                progress.update(repeats, advance=1, refresh=True)
+
+    table = make_table(header=("command", "numseqs", "k", "time(s)"), data=results)
+    print(table)
+    table.write(outpath)
 
 
 if __name__ == "__main__":
-    main()
+    run()
