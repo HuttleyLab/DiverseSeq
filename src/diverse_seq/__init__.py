1836fcabfaee322686109b24c61d6120fdefb21d
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-08-26 12:04:30 +1000

    DOC: fix docstring and bump version 2024.8.26a4

--- src/diverse_seq/__init__.py
+++ src/diverse_seq/__init__.py
@@ -1,7 +1,7 @@
-"""divergent: a tool for sampling biological sequences"""
+"""diverse_seq: a tool for sampling diverse biological sequences"""
 
 # need to import hdf5plugin here to make sure the plugin path can be
 # found by h5py
 import hdf5plugin  # noqa
 
-__version__ = "2024.8.26a3"
+__version__ = "2024.8.26a4"
