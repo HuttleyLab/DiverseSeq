e888b2eb22d4fea1081e920a9bdb488e041608a6
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-09-11 15:59:00 +1000

    ENH: `records_from_seq_store` returns records ordered by those given in `seq_names`

--- src/diverse_seq/records.py
+++ src/diverse_seq/records.py
@@ -502,7 +502,12 @@ def records_from_seq_store(
     """
     dstore = dvs_data_store.HDF5DataStore(seq_store, mode="r")
     make_record = member_to_kmerseq(k=k, moltype=moltype)
-    records = [make_record(m) for m in dstore.completed if m.unique_id in seq_names]  # pylint: disable=not-callable
+    records = {
+        m.unique_id: make_record(m)
+        for m in dstore.completed
+        if m.unique_id in seq_names
+    }  # pylint: disable=not-callable
+    records = [records[name] for name in seq_names]
     records = records[:limit] if limit else records
     for record in records:
         if not record:
