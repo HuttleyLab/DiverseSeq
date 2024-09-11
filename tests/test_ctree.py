e80701b7ede02d8aaca7527b4c9858cc17c53c32
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-09-11 16:13:42 +1000

    TST: add parallel test for cluster tree

--- tests/test_ctree.py
+++ tests/test_ctree.py
@@ -21,6 +21,20 @@ def test_ctree_mash(DATA_DIR, sketch_size):
     assert tree.same_topology(expected)
 
 
+def test_ctree_mash_parallel(DATA_DIR):
+    dvseqs_path = DATA_DIR / "brca1.dvseqs"
+    app = dvs_cluster_tree(
+        seq_store=dvseqs_path,
+        k=16,
+        sketch_size=400,
+        numprocs=4,
+    )
+
+    tree = app(["Human", "Chimpanzee", "Manatee", "Dugong", "Rhesus"])
+    expected = make_tree("(((Human, Chimpanzee), Rhesus), (Manatee, Dugong));")
+    assert tree.same_topology(expected)
+
+
 def test_ctree_euclidean(DATA_DIR):
     dvseqs_path = DATA_DIR / "brca1.dvseqs"
     app = dvs_cluster_tree(seq_store=dvseqs_path, k=5, distance_mode="euclidean")
